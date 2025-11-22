ca7c082619324dfa6f5081071d40a6d46ece8130
Benni-temp-Laptop <benjam.winkler@gmail.com>
2025-11-22 21:06:09 +0100

    Session

---
+++ dotnet-restful-media-review-server/System/Session.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_restful_media_review_server.System
+{
+    public sealed class Session
+    {
+        private const string _ALPHABET = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private static readonly Dictionary<string, Session> _Sessions = new();
+
+        private Session(string userName, string password)
+        {
+            UserName = userName;
+            IsAdmin = (userName == "admin");
+
+            Token = string.Empty;
+            Random rnd = new();
+            for (int i = 0; i < 24; i++) { Token += _ALPHABET[rnd.Next(0, 62)]; }
+        }
+
+
+        public string Token { get; }
+
+        public string UserName { get; }
+
+        public bool Valid
+        {
+            get { return _Sessions.ContainsKey(Token); }
+        }
+
+        public bool IsAdmin { get; }
+
+        public void Close()
+        {
+            lock (_Sessions)
+            {
+                if (_Sessions.ContainsKey(Token)) { _Sessions.Remove(Token); }
+            }
+        }
+    }
+}
