b26702eb0411fccae9c912b75cf024d20ecb0d29
Benni <benjam.winkler@gmail.com>
2025-11-23 11:55:14 +0100

    Atom base class for data objects

---
+++ dotnet-restful-media-review-server/System/Atom.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_restful_media_review_server.System
+{
+    //base implementation for data objects
+    public abstract class Atom : IAtom
+    {
+        protected Session? _EditingSession = null;
+
+
+        protected void _VerifySession(Session? session = null)
+        {
+            if (session is not null) { _EditingSession = session; }
+            if (_EditingSession is null || !_EditingSession.Valid) { throw new UnauthorizedAccessException("Invalid session."); }
+        }
+
+        protected void _EndEdit()
+        {
+            _EditingSession = null;
+        }
+
+        protected void _EnsureAdmin()
+        {
+            _VerifySession();
+            if (!_EditingSession!.IsAdmin) { throw new UnauthorizedAccessException("Admin privileges required."); }
+        }
+
+
+        protected void _EnsureAdminOrOwner(string owner)
+        {
+            _VerifySession();
+            if (!(_EditingSession!.IsAdmin || (_EditingSession.UserName == owner)))
+            {
+                throw new UnauthorizedAccessException("Admin or owner privileges required.");
+            }
+        }
+
+
+        public virtual void BeginEdit(Session session)
+        {
+            _VerifySession(session);
+        }
+
+        public abstract void Save();
+
+        public abstract void Delete();
+
+        public abstract void Refresh();
+    }
+}
---
+++ dotnet-restful-media-review-server/System/IAtom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_restful_media_review_server.System
+{
+    public interface IAtom
+    {
+        public void BeginEdit(Session session);
+
+        public void Save();
+
+        public void Delete();
+
+        public void Refresh();
+    }
+}
