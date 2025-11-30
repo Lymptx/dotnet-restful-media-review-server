40064a7b8fe736607bb434681284286a6237f665
Benni <benjam.winkler@gmail.com>
2025-11-30 11:57:38 +0100

    Rating model

---
+++ dotnet-restful-media-review-server/System/Rating.cs
@@ -0,0 +1,29 @@
+﻿using dotnet_restful_media_review_server.System;
+
+namespace dotnet_restful_media_review_server.System
+{
+    public sealed class Rating : Atom
+    {
+        public string Id { get; private set; } = Guid.NewGuid().ToString();
+        public string MediaId { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+
+        public int Stars { get; set; }
+        public string Comment { get; set; } = string.Empty;
+
+        public override void Save()
+        {
+            _EndEdit();
+        }
+
+        public override void Delete()
+        {
+            _EndEdit();
+        }
+
+        public override void Refresh()
+        {
+            // Later used for DB load
+        }
+    }
+}
